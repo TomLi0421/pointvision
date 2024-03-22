04910d893fd30243714017c228a753988538fb55
Tom <tom.cyli50@gmail.com>
2024-03-22 15:33:08 +0800

    feat: change the btn bg-color in product detail page and shopping cart page

--- client/src/pages/ShoppingCart/index.tsx
+++ client/src/pages/ShoppingCart/index.tsx
@@ -4,12 +4,13 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import ShoppingCartItemList from "../../components/ShoppingCartItemList/ShoppingCartItemList";
+import Button from "../../components/ui/Button";
 
 function ShoppingCartPage() {
   const { shoppingCartProduct } = useContext(ShoppingCartContext);
 
   return (
-    <div className="p-6 md:max-w-[38rem] md:m-auto">
+    <div className="p-6 md:py-32 md:max-w-[38rem] md:m-auto">
       <Link
         to="/product"
         className={`${styles.shopping_cart__text_color} font-semibold text-lg`}
@@ -27,6 +28,11 @@ function ShoppingCartPage() {
         You have {shoppingCartProduct.length} item in your cart
       </p>
       <ShoppingCartItemList />
+      <Button
+        className={`${styles.shopping_cart__btn_bg_color} text-white px-20 py-2.5 rounded mt-8 w-full`}
+      >
+        Checkout
+      </Button>
     </div>
   );
 }
